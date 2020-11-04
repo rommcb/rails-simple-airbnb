bff4fe2a67f5b736aea3d5787f3fa5029bb1d446
Romain Borremans <romain.borremans@gmail.com>
2020-11-04 16:21:39 +0100

    styled index view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Super Flat Brussels',
+  address: '10 blabla street Brussels',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 100,
+  number_of_guests: 6
+)
+
+Flat.create!(
+  name: 'Light & Spacious Flat in Waterloo',
+  address: 'rue vandelanotte, 3 Waterloo',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 65,
+  number_of_guests: 4
+)
+
+Flat.create!(
+  name: 'BOUM Fat Zee Flat',
+  address: 'aan de zee straat, 4, Oostende',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 89,
+  number_of_guests: 6
+)
